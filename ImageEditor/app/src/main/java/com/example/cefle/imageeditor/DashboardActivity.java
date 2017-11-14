add839f99af4ebbe32498dcd5299c571046bc5cd
Zrezi <zbrezi@gmail.com>
2017-11-14 12:30:14 -0600

    minor changes to UI in DashboardActivity

--- ImageEditor/app/src/main/java/com/example/cefle/imageeditor/DashboardActivity.java
+++ ImageEditor/app/src/main/java/com/example/cefle/imageeditor/DashboardActivity.java
@@ -1,6 +1,8 @@
 package com.example.cefle.imageeditor;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,8 +11,10 @@ import android.widget.ImageButton;
 public class DashboardActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_SELECT_FROM_GALLERY = 100;
+    private static final int REQUEST_CODE_CAPTURE_IMAGE = 200;
 
     private ImageButton selectFromGalleryButton;
+    private ImageButton captureImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +27,11 @@ public class DashboardActivity extends AppCompatActivity {
 
     private void findViews() {
         selectFromGalleryButton = (ImageButton) findViewById(R.id.btn_select_from_gallery);
+        captureImageButton = (ImageButton) findViewById(R.id.btn_capture_image);
     }
 
     private void attachListeners() {
+
         selectFromGalleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,6 +40,14 @@ public class DashboardActivity extends AppCompatActivity {
                 startActivityForResult(pickerIntent, REQUEST_CODE_SELECT_FROM_GALLERY);
             }
         });
+
+        captureImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO
+            }
+        });
+
     }
 
     @Override
@@ -42,8 +56,17 @@ public class DashboardActivity extends AppCompatActivity {
 
         if (requestCode == REQUEST_CODE_SELECT_FROM_GALLERY) {
             if (resultCode == RESULT_OK) {
+
+                // Create the intent to send the image to the ImageEditActivity
                 Intent imageEditIntent = new Intent(this, ImageEditActivity.class);
-                imageEditIntent.setData(data.getData());
+
+                // Extract the image path Uri from the result Intent
+                Uri imagePath = data.getData();
+
+                // Set the new Intent's data to the image path
+                imageEditIntent.setData(imagePath);
+
+                // Start the activity
                 startActivity(imageEditIntent);
             }
         }
